07333161276188d915c5ce51648752301b285211
Jason Walton <dev@lucid.thedreaming.org>
2025-10-02 22:24:29 -0400

    docs: Document fields in `DownloadResult`.

--- src/handles/result.rs
+++ src/handles/result.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
+
 /// The result of a download operation.
 #[derive(Debug, Clone)]
 #[non_exhaustive]
 pub struct DownloadResult {
+    /// The total number of tries we made to download the file.
     pub tries: u64,
+    /// The path the file was saved to.
     pub path: PathBuf,
+    /// The total number of bytes downloaded.
     pub bytes_downloaded: u64,
 }
