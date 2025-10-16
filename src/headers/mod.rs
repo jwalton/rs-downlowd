3f0c2e8a8dd47b60155979ca8b87acc5c5f8514d
Jason Walton <dev@lucid.thedreaming.org>
2025-10-16 19:27:27 -0400

    refator: Simplify header parsing.

--- src/headers/mod.rs
+++ src/headers/mod.rs
@@ -13,36 +13,31 @@ fn get_header_str<'a>(headers: &'a HeaderMap, name: &HeaderName) -> Option<&'a s
 
 /// Parses the `Content-Length` header value into a `u64`.
 pub fn parse_content_length(headers: &HeaderMap) -> Option<u64> {
-    get_header_str(headers, &reqwest::header::CONTENT_LENGTH).and_then(parse_content_length_str)
-}
-
-fn parse_content_length_str(value: &str) -> Option<u64> {
-    value.parse().ok()
+    get_header_str(headers, &reqwest::header::CONTENT_LENGTH).and_then(|value| value.parse().ok())
 }
 
 /// Parses the filename from the `Content-Disposition` header.
 pub fn parse_content_disposition(headers: &'_ HeaderMap) -> Option<Cow<'_, str>> {
-    get_header_str(headers, &reqwest::header::CONTENT_DISPOSITION)
-        .and_then(parse_content_disposition_str)
-}
+    if let Some(value) = get_header_str(headers, &reqwest::header::CONTENT_DISPOSITION) {
+        let mut result: Option<Cow<str>> = None;
 
-fn parse_content_disposition_str(value: &'_ str) -> Option<Cow<'_, str>> {
-    let mut result: Option<Cow<str>> = None;
+        value.split(';').for_each(|part| {
+            let trimmed = part.trim();
+            result = if trimmed.starts_with("filename=") && result.is_none() {
+                Some(Cow::Borrowed(
+                    trimmed.trim_start_matches("filename=").trim_matches('"'),
+                ))
+            } else if trimmed.starts_with("filename*=UTF-8''") {
+                urlencoding::decode(trimmed.trim_start_matches("filename*=UTF-8''")).ok()
+            } else {
+                None
+            }
+        });
 
-    value.split(';').for_each(|part| {
-        let trimmed = part.trim();
-        result = if trimmed.starts_with("filename=") && result.is_none() {
-            Some(Cow::Borrowed(
-                trimmed.trim_start_matches("filename=").trim_matches('"'),
-            ))
-        } else if trimmed.starts_with("filename*=UTF-8''") {
-            urlencoding::decode(trimmed.trim_start_matches("filename*=UTF-8''")).ok()
-        } else {
-            None
-        }
-    });
-
-    result
+        result
+    } else {
+        None
+    }
 }
 
 pub fn etag(headers: &HeaderMap) -> Option<&str> {
@@ -56,31 +51,48 @@ pub fn get_last_modified(headers: &HeaderMap) -> Option<&str> {
 
 #[cfg(test)]
 mod tests {
+    use http::HeaderValue;
+
     use super::*;
+
+    fn make_header(k: &'static str, v: &'static str) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+        headers.append(k, HeaderValue::from_static(v));
+        headers
+    }
 
     #[test]
     fn should_parse_content_length() {
-        let length = parse_content_length_str("12345");
+        let length = parse_content_length(&make_header("content-length", "12345"));
         assert_eq!(length, Some(12345));
 
-        let length = parse_content_length_str("some nonsense");
+        let length = parse_content_length(&make_header("content-length", "some nonsense"));
         assert_eq!(length, None);
     }
 
     #[test]
     fn should_parse_content_disposition() {
-        let filename = parse_content_disposition_str(r#"attachment; filename="example.txt""#);
+        let header = make_header(
+            "content-disposition",
+            r#"attachment; filename="example.txt""#,
+        );
+        let filename = parse_content_disposition(&header);
         assert_eq!(filename, Some("example.txt".into()));
 
-        let filename =
-            parse_content_disposition_str(r#"attachment; filename*=UTF-8''file%20name.jpg"#);
+        let header = make_header(
+            "content-disposition",
+            r#"attachment; filename*=UTF-8''file%20name.jpg"#,
+        );
+        let filename = parse_content_disposition(&header);
         assert_eq!(filename, Some("file name.jpg".into()));
 
         // If there's a filename and a filename* in the same header,
         // filename* should take precedence.
-        let filename = parse_content_disposition_str(
+        let header = make_header(
+            "content-disposition",
             r#"attachment; filename="foo.txt"; filename*=UTF-8''file%20name.jpg"#,
         );
+        let filename = parse_content_disposition(&header);
         assert_eq!(filename, Some("file name.jpg".into()));
     }
 }
